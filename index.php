887fa8fb18eb2b07df0c9f3f8285318dda1fdd76
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-14 21:23:26 +0800

    php added

--- index.php
+++ index.php
@@ -17,28 +17,7 @@
 </head>
 
 <body>
-  <!--Navigation bar-->
-  <nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.html"><span>CodeLy</span></a>
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href=""></a></li>
-          <li><a href="#courses">Курсууд</a></li>
-          <li><a href="#" data-target="#login" data-toggle="modal">Нэвтрэх</a></li>
-          
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!--/ Navigation bar-->
+  <?php include "navbar.php" ?>
   <!--Modal box-->
   <div class="modal fade" id="login" role="dialog">
     <div class="modal-dialog modal-sm">
---
+++ navbar.php
@@ -0,0 +1,22 @@
+ <!--Navigation bar-->
+ <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.html"><span>CodeLy</span></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href=""></a></li>
+          <li><a href="#courses">Курсууд</a></li>
+          <li><a href="#" data-target="#login" data-toggle="modal">Нэвтрэх</a></li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Navigation bar-->
