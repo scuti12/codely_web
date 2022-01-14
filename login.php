f02030f5119158536348e9e0b8427bba4853029b
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-14 22:29:58 +0800

    fix

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "011011";
 $dbname = "CODELY";
-
+$email = $_POST['loginid']
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
@@ -11,15 +11,16 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM user where email=$_POST['loginid']";
+$sql = "SELECT `password` FROM `user` WHERE `email`= '$email' ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-   if($row['password'==$_POST['loginpsw']]){
-       $_SESSION['email']=$_POST['loginid'];
-   }
+      if(isset($row['password'])){
+          $_SESSION['email'] = $email;
+          header("location: index.php");
+      }
   }
 } else {
   echo "Та админд хандана уу";
