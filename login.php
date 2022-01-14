e94edb5a510ebea80bc897aa9918cc00efe9faf2
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-14 21:57:19 +0800

    added connect wuare

--- login.php
+++ login.php
@@ -1,16 +1,24 @@
 <?php
-
-
 $servername = "localhost";
 $username = "root";
 $password = "011011";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$dbname="CODELY"
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-?> 
+
+$sql = "SELECT * from FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["email"]. $row["password"] "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
