47757ae8f96c9a3994915a1c36aa28acd0aaa350
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 09:18:16 +0800

    asd

--- cpp.php
+++ cpp.php
@@ -21,7 +21,11 @@ session_start();
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <body>
 <?php include "navbar_logged.php" ?>
-<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
   <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
   <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
   <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
@@ -33,11 +37,6 @@ session_start();
   <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
   <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
 </div>
-
-    <br>
-    <br>
-    <br>
-    <br>
     <br>
     <br>
     <br>
