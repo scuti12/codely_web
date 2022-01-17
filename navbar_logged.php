47c6bbc1ff99fccf60a090c7444ac2a518d9da18
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 09:04:03 +0800

    fixed

---
+++ navbar_logged.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"><span>CodeLy</span></a>
+      </div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href=""></a></li>
+          <li><a href="index.php">Курсууд</a></li>
+          <li><a href="#" data-target="" ><?php session_start(); echo($_SESSION['username']) ?></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
