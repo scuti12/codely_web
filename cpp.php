a0ef0e145bac4757e889392dcc83403a50a162fb
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 09:23:37 +0800

    asd

--- cpp.php
+++ cpp.php
@@ -25,17 +25,33 @@ session_start();
     <br>
     <br>
     <br>
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-  <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-  <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-  <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-  <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+    <div class="row">
+<div class="col-3">
+<nav class ="navbar bg-dark">
+<ul class ="nav navbar-nav">
+<li class ="nav-item">
+<a class ="nav-link" href="#"> Home </a>
+</li>
+<li class ="nav-item">
+<a class ="nav-link" href="#"> Services </a>
+</li>
+<li class ="nav-item">
+<a class ="nav-link" href="#"> Contact </a>
+</li>
+<li class ="nav-item">
+<a class ="nav-link" href="#"> Blogs </a>
+</li>
+</ul>
+</nav>
 </div>
-<div class="tab-content" id="v-pills-tabContent">
-  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+<div class="col-9">
+<div class="container">
+The Web Content in detail.
+</div>
+<div class =”container”>
+<p> The vertical menu can place the left or right side of the web pages. <br> But the default vertical menu placed on the left side. </P>
+</div>
+</div>
 </div>
     <br>
     <br>
