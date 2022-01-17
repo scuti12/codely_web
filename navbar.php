3d3969f4a66914f7d4a71bfd0f6b4de69431d580
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 18:03:57 +0800

    asd

--- navbar.php
+++ navbar.php
@@ -29,7 +29,7 @@
                     } 
           ?></a></li>
           <li><a href="logout.php"><? session_start();
-          if($_SESSION['username']!==""){
+          if($_SESSION['username']!=""){
                       echo"ГАРАХ";
                     }?></a></li>
         </ul>
