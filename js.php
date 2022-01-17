2c5be9021b9146c448c5172a6a3d7421586c0b6d
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 14:13:00 +0800

    desc fixed

--- js.php
+++ js.php
@@ -11,9 +11,8 @@ session_start();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Javascript | Codely</title>
-  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
-  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
-
+  <meta name="description" content="Codely MN | learn easily">
+  <meta name="keywords" content="Programm developer and Web developer">
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
   <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
