c484492ac51838b608b7a907078277bba7f8abbe
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-17 08:42:43 +0800

    added video

--- cpp.php
+++ cpp.php
@@ -21,8 +21,10 @@ session_start();
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <body>
 <?php include "navbar.php" ?>
-<h3 style="color:orange; text-align:center"> Үр дүн алга </h3>
-    <br>
+<div style="padding:56.16% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/666458985?h=3b6a6dc5da&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="syntax"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<div style="padding:56.16% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/666458822?h=3af095bd2e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="dataType"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<div style="padding:56.16% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/666458868?h=e2d094859e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="operator_operand"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<div style="padding:56.16% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/666458920?h=833f10826e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="string_int"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
     <br>
     <br>
     <br>
