9ba3302a05476c407a287f030dd1b8231e15b965
Deriilaamyatav <deriilaamyatav@asterisk-tech.mn>
2022-01-19 17:42:20 +0800

    scuti

--- cpp.php
+++ cpp.php
@@ -25,12 +25,7 @@ session_start();
 <iframe src="https://player.vimeo.com/video/666458822?h=3af095bd2e" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 <iframe src="https://player.vimeo.com/video/666458868?h=e2d094859e" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 <iframe src="https://player.vimeo.com/video/666458920?h=833f10826e" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe> -->
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    
     <nav class="menu">
   <ol class='menu__content'>
     <li class="menu-item"><a href="#0">Home</a></li>
@@ -54,7 +49,12 @@ session_start();
     <li class="menu-item"><a href="#0">Contact</a></li>
   </ol>
 </nav>
-
+<br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 <?php include "footer.php" ?>
 </body>
 </head>
